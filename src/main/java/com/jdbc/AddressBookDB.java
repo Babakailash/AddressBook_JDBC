0cdcb53b2daf680d43586086a986209bd336dd4c
Babakailash <nathkailash2020@gmail.com>
2021-05-30 19:23:25 +0530

    Welcome JDBC AddressBook Connection

---
+++ src/main/java/com/jdbc/AddressBookDB.java
@@ -0,0 +1,7 @@
+package com.jdbc;
+
+public class AddressBookDB {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook JDBC Connection With Mysql");
+    }
+}
